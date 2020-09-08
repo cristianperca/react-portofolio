90b0cbf0265107e168d5e35bc7951b6db0b2fd9f
Cristian Perca <cristianperca@yahoo.com>
2020-09-08 19:19:31 +0200

    First build to Netlify

---
+++ build/precache-manifest.464726e5b70327dfc0b380904850081e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8a072917ea385147985a00c9309ed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd31344bcede21ed16ca",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "430f384398b057bca0ed",
+    "url": "/static/js/2.dda652dd.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.dda652dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd31344bcede21ed16ca",
+    "url": "/static/js/main.da7fa21c.chunk.js"
+  },
+  {
+    "revision": "0aa8b2578be25ed32d30",
+    "url": "/static/js/runtime-main.400d81cc.js"
+  }
+]);
